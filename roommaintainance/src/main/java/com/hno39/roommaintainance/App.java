d5df811b606d543e2f2e67a46660709775f9a519
Harish Bollina <harish3989@gmail.com>
2018-06-27 08:58:48 +0530

    roommaintainance project created

---
+++ roommaintainance/src/main/java/com/hno39/roommaintainance/App.java
@@ -0,0 +1,13 @@
+package com.hno39.roommaintainance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
